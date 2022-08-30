ebc980977c9b48c3d8757549e4b9738e7f4d8139
kemomi <76780905+kemomi@users.noreply.github.com>
2022-08-30 11:30:24 +0800

    Create user.js

---
+++ user.js
@@ -0,0 +1,35 @@
+{
+    "config": {
+        "key": "b", // 自定义 ( Ctrl / ⌘ ) + ？ 快捷键
+        "autoBionic": true, // 开启页面默认运行
+        "skipLinks": false, // 跳过链接
+        "skipWords": false, // 跳过特定单词
+        "scale": 0.5, // 单词高亮的长度比例
+        "maxBionicLength": null, // 单词最长高亮数量
+        "opacity": 1, // 高亮单词 透明度
+        "saccade": 0, // 间隔高亮 改成数字 1 即 隔一个单词高亮一个
+        "symbolMode": false, // 符号模式替换，替换原本字符 但 不影响 DOM 结构
+        "excludeWords": [ // 跳过的单词们，skipWords 不开启不生效
+            "is",
+            "as",
+            "if",
+            "the",
+            "of",
+            "to",
+            "be",
+            "for",
+            "this"
+        ],
+    }
+}
+
+## 什么是用户脚本？
+用户脚本是一段代码，它们能够优化您的网页浏览体验。安装之后，有些脚本能为网站添加新的功能，有些能使网站的界面更加易用。
+有些则能隐藏网站上烦人的部分内容。用户脚本都是由用户编写并向全世界发表的，您可以免费安装，轻松体验。
+
+#安装油猴  
+安装一个用户脚本管理器 
+SX
+
+## GitHub
+https://github.com/kemomi/reading.user.js
